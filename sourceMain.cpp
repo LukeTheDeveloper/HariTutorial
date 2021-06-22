81cc8947b4011d6f1460c2c821637e039599fc64
Luke Harrison <81396232+LukeTheDeveloper@users.noreply.github.com>
2021-06-22 15:18:54 +0100

    Create sourceMain.cpp

---
+++ sourceMain.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int age = 0;
+int main()
+{
+    cout << "Hello World! How old are you?\n";
+    cin >> age;
+    cout << "your age is " << age << endl;
+    cout << "Hi Hari How are you today????" << endl;
+
+    system("pause>0");
+}
